098022c58fc790fc7cfae9790d1fda047bf7e7da
MrShadowalker <mr.shadowalker@foxmail.com>
2020-11-03 14:56:05 +0800

    project init

---
+++ src/main/java/org/itron/itrain/ItrainApplication.java
@@ -0,0 +1,13 @@
+package org.itron.itrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItrainApplication.class, args);
+    }
+
+}
