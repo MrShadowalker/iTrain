f1499c3ade7e959854f2cbf350aa337b9d88e4cf
MrShadowalker <mr.shadowalker@foxmail.com>
2020-11-04 14:26:13 +0800

    基于 WebSocket 构建 P2P 网络

---
+++ src/main/java/org/itron/itrain/p2p/P2pPointClient.java
@@ -0,0 +1,110 @@
+package org.itron.itrain.p2p;
+
+import lombok.extern.slf4j.Slf4j;
+import org.java_websocket.WebSocket;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.testng.util.Strings;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基于 Spring Boot 2.0 的 WebSocket 客户端
+ *
+ * @author Shadowalker
+ */
+@Slf4j
+@Component
+public class P2pPointClient {
+    // P2P 网络中的节点既是服务端，又是客户端。作为服务端运行在 7770 端口
+    // 同时作为客户端通过 ws://localhost:7771 连接到服务端
+    private String wsUrl = "ws://localhost:7771/";
+
+    // 所有客户端 WebSocket 的连接池缓存
+    private List<WebSocket> localSockets = new ArrayList<>();
+
+    public List<WebSocket> getLocalSockets() {
+        return localSockets;
+    }
+
+    public void setLocalSockets(List<WebSocket> localSockets) {
+        this.localSockets = localSockets;
+    }
+
+    @PostConstruct
+    @Order(2)
+    public void connectPeer() {
+        try {
+            // 创建 WebSocket 的客户端
+            final WebSocketClient socketClient = new WebSocketClient(new URI(wsUrl)) {
+                @Override
+                public void onOpen(ServerHandshake serverHandshake) {
+                    sendMessage(this, "Shadow.Net 成功创建客户端。");
+                    localSockets.add(this);
+                }
+
+                @Override
+                public void onMessage(String message) {
+                    log.info("Shadow.Net 客户端收到服务端发送的消息：{}", message);
+                }
+
+                @Override
+                public void onClose(int i, String msg, boolean b) {
+                    log.info("Shadow.Net 客户端关闭。");
+                    localSockets.remove(this);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    log.info("Shadow.Net 客户端报错！");
+                    localSockets.remove(this);
+                }
+            };
+            // 客户端开始连接服务器
+            socketClient.connect();
+        } catch (URISyntaxException e) {
+            log.error("Shadow.Net 连接错误：{}", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 向服务端发送消息，当前 WebSocket 的远程 Socket 地址就是服务端
+     *
+     * @param webSocket
+     * @param message
+     */
+    private void sendMessage(WebSocket webSocket, String message) {
+        log.info("发送给" + webSocket.getRemoteSocketAddress().getPort() + "的 P2P 消息：{}", message);
+        webSocket.send(message);
+    }
+
+    /**
+     * 向所有连接过的服务端广播消息
+     *
+     * @param message 待广播的消息
+     */
+    private void broadcast(String message) {
+        if (localSockets.isEmpty() || Strings.isNullOrEmpty(message)) {
+            return;
+        }
+        log.info("Glad to say broadcast to servers being started !");
+        for (WebSocket socket : localSockets) {
+            this.sendMessage(socket, message);
+        }
+        log.info("Glad to say broadcast to servers has done !");
+    }
+
+    /********** 使用方法 **********/
+
+    // 代码编写完成后，在工程目录下运行 mvn clean package 命令，将 itrain 工程打包成 itrain.jar
+    // 随后切换到 target 目录，执行 java -jar itrain.jar
+    // ……
+    // 日志前两行是节点作为 Server 端输出，下面是节点作为客户端和服务端互相发送的消息。
+}
---
+++ src/main/java/org/itron/itrain/p2p/P2pPointServer.java
@@ -0,0 +1,141 @@
+package org.itron.itrain.p2p;
+
+import lombok.extern.slf4j.Slf4j;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.testng.util.Strings;
+
+import javax.annotation.PostConstruct;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基于 Spring Boot 2.0 的 WebSocket 服务端
+ *
+ * @author Shadowalker
+ */
+@Slf4j
+@Component
+public class P2pPointServer {
+    // 本机 Server 的 WebSocket 端口
+    // 多机测试时可改变该值
+    private int port = 7770;
+
+    // 所有连接到服务端的 WebSocket 缓存器
+    private List<WebSocket> localSockets = new ArrayList<>();
+
+    public List<WebSocket> getLocalSockets() {
+        return localSockets;
+    }
+
+    public void setLocalSockets(List<WebSocket> localSockets) {
+        this.localSockets = localSockets;
+    }
+
+    /**
+     * 初始化 P2P Server 端
+     */
+    @PostConstruct
+    @Order(1)
+    public void initServer() {
+        /**
+         * 初始化 WebSocket 的服务器定义内部类对象 socketServer，源于 WebSocketServer
+         * new InetSocketAddress(port) 是 WebSocketServer 构造器的参数
+         * InetSocketAddress 是（IP 地址 + 端口号）类型，即端口地址类型
+         */
+        final WebSocketServer socketServer = new WebSocketServer(new InetSocketAddress(port)) {
+
+            /***** 重写 5 个事件方法，事件发生时触发对应的方法 *****/
+
+            /**
+             * 创建连接成功时触发
+             * @param webSocket
+             * @param clientHandshake
+             */
+            @Override
+            public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+                sendMessage(webSocket, "Shadow.Net 服务端成功创建连接");
+                // 当成功创建一个 WebSocket 连接时，将该连接加入连接池
+                localSockets.add(webSocket);
+            }
+
+            /**
+             * 断开连接时触发
+             * @param webSocket
+             * @param i
+             * @param s
+             * @param b
+             */
+            @Override
+            public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+                log.info(webSocket.getRemoteSocketAddress() + " 客户端与服务器断开连接！");
+                // 当客户端断开连接时，WebSocket 连接池删除该连接
+                localSockets.remove(webSocket);
+            }
+
+            /**
+             * 收到客户端发来的消息时触发
+             * @param webSocket
+             * @param msg
+             */
+            @Override
+            public void onMessage(WebSocket webSocket, String msg) {
+                log.info("Shadow.Net 服务端接收到客户端消息：{}", msg);
+                sendMessage(webSocket, "收到消息");
+            }
+
+            /**
+             * 连接发生错误时调用，紧接着触发 onClose 方法
+             * @param webSocket
+             * @param e
+             */
+            @Override
+            public void onError(WebSocket webSocket, Exception e) {
+                log.info(webSocket.getRemoteSocketAddress() + " 客户端连接错误！");
+                localSockets.remove(webSocket);
+            }
+
+            /**
+             * 服务端启动
+             */
+            @Override
+            public void onStart() {
+                log.info("Shadow.Net 的 WebSocket Server 端启动……");
+            }
+        };
+
+        socketServer.start();
+        log.info("Shadow.Net 服务端监听 socketServer 端口：{}", port);
+    }
+
+    /**
+     * 向连接到本机的某客户端发送消息
+     *
+     * @param ws
+     * @param message
+     */
+    private void sendMessage(WebSocket ws, String message) {
+        log.info("发送给" + ws.getRemoteSocketAddress().getPort() + "的 P2P 消息是：" + message);
+        ws.send(message);
+    }
+
+    /**
+     * 向所有连接到本机的客户端广播消息
+     *
+     * @param message
+     */
+    public void broadcast(String message) {
+        if (localSockets.isEmpty() || Strings.isNullOrEmpty(message)) {
+            return;
+        }
+        log.info("Glad to say broadcast to clients being started !");
+        for (WebSocket socket : localSockets) {
+            this.sendMessage(socket, message);
+        }
+        log.info("Glad to say broadcast to clients has done !");
+    }
+}
